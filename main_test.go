3d1da6150ef46c6b6fa35b53ab1584ce67f48da7
Drincann <1019933576@qq.com>
2022-03-04 23:45:42 +0800

    chore: post put delete 的测试

--- main_test.go
+++ main_test.go
@@ -13,14 +13,106 @@ const (
 	port = 8080
 )
 
-func TestBlack(t *testing.T) {
+type StringReader string
+
+func (str StringReader) Read(p []byte) (n int, err error) {
+	return copy(p, []byte(str)), nil
+}
+
+func init() {
 	app := angie.New()
-	go app.Get("/", func(ctx *angie.Context) {
+	go app.Post("/", func(ctx *angie.Context) {
+		ctx.Res.Body = "Hello World!"
+	}).Get("/", func(ctx *angie.Context) {
+		ctx.Res.Body = "Hello World!"
+	}).Put("/", func(ctx *angie.Context) {
+		ctx.Res.Body = "Hello World!"
+	}).Delete("/", func(ctx *angie.Context) {
 		ctx.Res.Body = "Hello World!"
 	}).Listen(port)
+}
 
-	res, err := http.Get(fmt.Sprintf("http://localhost:%d/?name=Drincann", port))
+func TestGet(t *testing.T) {
 
+	res, err := http.Get(fmt.Sprintf("http://localhost:%d", port))
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, res.StatusCode)
+	}
+
+	// read response
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(body) != "Hello World!" {
+		t.Errorf("Expected body 'Hello World!', got '%s'", string(body))
+	}
+}
+
+func TestPost(t *testing.T) {
+
+	res, err := http.Post(fmt.Sprintf("http://localhost:%d", port), "application/json", StringReader("{}"))
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, res.StatusCode)
+	}
+
+	// read response
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(body) != "Hello World!" {
+		t.Errorf("Expected body 'Hello World!', got '%s'", string(body))
+	}
+}
+
+func TestPut(t *testing.T) {
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPut, fmt.Sprintf("http://localhost:%d", port), StringReader("{}"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, res.StatusCode)
+	}
+
+	// read response
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(body) != "Hello World!" {
+		t.Errorf("Expected body 'Hello World!', got '%s'", string(body))
+	}
+}
+
+func TestDelete(t *testing.T) {
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("http://localhost:%d", port), StringReader("{}"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	res, err := client.Do(req)
 	if err != nil {
 		t.Error(err)
 	}
