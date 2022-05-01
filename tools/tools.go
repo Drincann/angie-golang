0088c643681b89a5da2f4b8cc7e1e60b72423d2e
Drincann <1019933576@qq.com>
2022-05-01 23:58:28 +0800

    feature: 实现了 tools.IfElse 三元运算

--- tools/tools.go
+++ tools/tools.go
@@ -5,6 +5,14 @@ import (
 	"runtime"
 )
 
+func IfElse[T any](condition bool, trueVal T, falseVal T) T {
+	if condition {
+		return trueVal
+	} else {
+		return falseVal
+	}
+}
+
 func OpenBrowser(url string) (stdout string, err error) {
 	stdoutBytes, e := exec.Command(
 		map[string]string{"windows": "cmd /c start", "darwin": "open"}[runtime.GOOS],
